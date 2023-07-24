8d820143041ff6aa38da13eb5a46ea331f570f4c
jucamilo340 <jucamilo340@gmail.com>
2023-07-24 11:00:07 -0500

    update responsive

--- src/scenes/Login/index.tsx
+++ src/scenes/Login/index.tsx
@@ -35,7 +35,7 @@ const Login: React.FC = () => {
   };
 
   return (
-    <div className="flex flex-col md:flex-row h-screen items-center">
+    <div className="flex flex-col lg:flex-row h-screen items-center">
       <div className="bg-indigo-600 hidden lg:block w-full md:w-1/2 xl:w-2/3 h-screen">
         <img src={backGround} alt="" className="w-full h-full object-cover" />
       </div>
